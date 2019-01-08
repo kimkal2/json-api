790758be5354da031bcecf76163962bab79b864f
Kim Kalhee <kimkal@rdu-c4b3.rnclab.us.alcatel-lucent.com>
2019-01-08 17:09:49 -0500

    seems working

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+        "fmt"
+        "github.com/julienschmidt/httprouter"
+        "log"
+        "net/http"
+)
+
+type Server struct {
+   r *httprouter.Router
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+   w.Header().Set("Content-Type", "application/json; charset=utf-8")
+   s.r.ServeHTTP(w, r)
+}
+
+func ListTasks(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+        fmt.Fprint(w, "ListTasks\n")
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+        fmt.Fprint(w, "CreateTasks\n")
+}
+
+func ReadTask(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+        fmt.Fprint(w, "CreateTasks\n")
+}
+
+func UpdateTask(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+        fmt.Fprint(w, "CreateTasks\n")
+}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+        fmt.Fprint(w, "UpdateTasks\n")
+}
+
+func main() {
+        router := httprouter.New()
+        router.GET("/", ListTasks)
+        router.POST("/", CreateTask)
+        router.GET("/:id", ReadTask)
+        router.PUT("/:id", UpdateTask)
+        router.DELETE("/:id", DeleteTask)
+
+        log.Fatal(http.ListenAndServe(":8080", router))
+}
